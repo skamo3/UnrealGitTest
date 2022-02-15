7ded179f5d3f8753a851af11dbc60d0f2c103355
skamo3 <54701846+skamo3@users.noreply.github.com>
2022-02-16 00:53:36 +0900

    TestPlust

---
+++ Source/GitTestPrj/TestPlus.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestPlus.h"
+
+// Sets default values
+ATestPlus::ATestPlus()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATestPlus::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestPlus::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
