6901d8027be181a4b657d053cfc52a4f9ecc8eb9
skamo3 <54701846+skamo3@users.noreply.github.com>
2022-02-16 00:11:47 +0900

    Test01 add

---
+++ Source/GitTestPrj/Test01.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class GITTESTPRJ_API Test01
+{
+public:
+	Test01();
+	~Test01();
+};
